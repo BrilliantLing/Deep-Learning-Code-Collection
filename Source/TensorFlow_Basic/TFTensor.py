31a59441f4580c117ca83326fc44369f512c2e4b
BrilliantLing <name321@qq.com>
2016-10-17 22:32:09 +0800

    fix bugs

--- Source/TensorFlow_Basic/TFTensor.py
+++ Source/TensorFlow_Basic/TFTensor.py
@@ -21,7 +21,7 @@ with tf.Session() as sess:
     # 运行 'init' op
     sess.run(init_op)
     print sess.run(state)
-    for _ in range(3)
+    for _ in range(3):
     # 运行 op, 更新 'state', 并打印 'state'
         sess.run(update)
         print sess.run(state)
