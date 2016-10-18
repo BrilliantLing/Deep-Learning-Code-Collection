452b422122ce5671df609408f20b557b8accc269
BrilliantLing <name321@qq.com>
2016-10-18 09:44:31 +0800

    fix somebugs

--- Source/TensorFlow_Basic/TFFeed.py
+++ Source/TensorFlow_Basic/TFFeed.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 import tensorflow as tf
 
-input1 = tf.placeholder(tf.types.float32)
-input2 = tf.placeholder(tf.types.float32)
+input1 = tf.placeholder(tf.float32)
+input2 = tf.placeholder(tf.float32)
 output = tf.mul(input1,input2)
 
 with tf.Session as sess:
