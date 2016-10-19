9e82f036a1495984a4ff3bd14c4ee6ca4f2d4656
BrilliantLing <name321@qq.com>
2016-10-19 19:10:55 +0800

    create new py

---
+++ Source/MNIST/mnist_cnn.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+import tensorflow as tf
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import argparse
+
