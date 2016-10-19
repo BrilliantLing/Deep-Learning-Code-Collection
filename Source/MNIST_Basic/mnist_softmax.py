ab78269458e68d8a28cb14ad44d2a304901d27dc
BrilliantLing <name321@qq.com>
2016-10-19 19:06:11 +0800

    add some comments

--- Source/MNIST_Basic/mnist_softmax.py
+++ Source/MNIST_Basic/mnist_softmax.py
@@ -51,6 +51,7 @@ def main(_):
   y = tf.matmul(x, W) + b
 
   # Define loss and optimizer
+  # y_是一个占位符，用于输入正确的答案
   y_ = tf.placeholder(tf.float32, [None, 10])
 
   # The raw formulation of cross-entropy,
@@ -66,17 +67,25 @@ def main(_):
   cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y, y_))
   # 使用梯度下降最小化交叉熵
   train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
-
+  #创建一个新的会话
   sess = tf.InteractiveSession()
-  # Train
+
+  # 训练模型
+  # 初始化所有的变量
   tf.initialize_all_variables().run()
+  # 训练1000次，使用批梯度下降，每次随机抓取训练数据中的100个批处理数据点
+  # 运行train_step，在其中用这些批数据点作为参数替换之前的的占位符
   for _ in range(1000):
     batch_xs, batch_ys = mnist.train.next_batch(100)
     sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
 
-  # Test trained model
+  # 测试训练好的模型
+  # tf.argmax返回tensor对象在某一维上的数据最大值的索引值
+  # correct_prediction是一个匹配布尔向量，y和y_两者相同则为1，不同为0
   correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+  # 计算准确率
   accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+  # 运行测试，输出结果
   print(sess.run(accuracy, feed_dict={x: mnist.test.images,
                                       y_: mnist.test.labels}))
 
