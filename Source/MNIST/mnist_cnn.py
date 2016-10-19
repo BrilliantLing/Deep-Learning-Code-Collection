b44269c3194ef42bdb810081ce7644d4d40929fb
TuXiang <name321@qq.com>
2016-10-19 19:54:31 +0800

    update on the vim,feeling so bad

--- Source/MNIST/mnist_cnn.py
+++ Source/MNIST/mnist_cnn.py
@@ -1,10 +1,31 @@
 # -*- coding: utf-8 -*-
 
-import tensorflow as tf
 
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
 import argparse
+
+from tensorflow.examples.tutorials.mnist import input_data
+
+import tensorflow as tf
+
+FLAG = None
+
+def weight_variable(shape):
+    initial = tf.truncated_normal(shape,stddev=0.1)
+    return tf.Variable(initial)
+
+def bias_variable(shape):
+    initial = tf.constant(0.1,shape=shape)
+    return tf.Variable(intial)
+
+def conv2d(x,W):
+    return tf.nn.conv2d(x,W,stride=[1,1,1,1],padding='SAME')
+
+def max_pool_2x2(x):
+    return tf.nn.max_pool(x,ksize=[1,2,2,1],strides=[1,2,2,1],padding='SAME')
+
+
 
