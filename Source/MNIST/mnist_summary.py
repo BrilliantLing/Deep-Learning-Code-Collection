81ab1c0157289942f85bbd48e04cb83b81c08a8f
BrilliantLing <name321@qq.com>
2016-10-20 20:21:45 +0800

    update the times of trainning

--- Source/MNIST/mnist_summary.py
+++ Source/MNIST/mnist_summary.py
@@ -188,7 +188,7 @@ def main(_):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--fake_data', nargs='?', const=True, type=bool,default=False,help='If true, uses fake data for unit testing.')
-    parser.add_argument('--max_steps', type=int, default=1000,help='Number of steps to run trainer.')
+    parser.add_argument('--max_steps', type=int, default=10000,help='Number of steps to run trainer.')
     parser.add_argument('--learning_rate', type=float, default=0.001,help='Initial learning rate')
     parser.add_argument('--dropout', type=float, default=0.9,help='Keep probability for training dropout.')
     parser.add_argument('--data_dir', type=str, default='/tmp/data',help='Directory for storing data')
