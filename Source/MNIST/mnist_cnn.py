571c4d5d0f42f02bbcc36745b1425e22649ff4aa
BrilliantLing <name321@qq.com>
2016-10-22 16:37:40 +0800

    fix bug

--- Source/MNIST/mnist_cnn.py
+++ Source/MNIST/mnist_cnn.py
@@ -66,7 +66,7 @@ def main(_):
             print("step %d, training accuracy %g" %(i, train_accuracy))
         train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
 
-    print "test accuracy %g" %accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0})
+    print("test accuracy %g" %accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
 
 
 if __name__ == '__main__':
