2c4490e94c11f8c3e4a0ceaab7778857389a61cd
TuXiang <name321@qq.com>
2016-11-25 21:38:50 +0800

    modified:   Source/Bao/ReadFiles.py

--- Source/Bao/ReadFiles.py
+++ Source/Bao/ReadFiles.py
@@ -1,12 +1,12 @@
 import os
 import tensorflow as tf
-import PIL import Image
+from PIL import Image
 
 cwd = os.getcwd()
 
 def create_record():
-    writer = tf.python_io.TFRecordWriter("train,tfrecords")
-    for index, name in enumerate(classes):
+    writer = tf.python_io.TFRecordWriter("train.tfrecords")
+    for index, name in enumerate():
         class_path = cwd + name + "/"
         for img_name in os.listdir(class_path):
             img_path = class_path + img_name
@@ -24,4 +24,33 @@ def read_and_decode(filename):
     filename_queue = tf.trian.string_input_producer([filename])
 
     reader = tf.TFRecordReader()
-    _, serialized_example = reader
+    _, serialized_example = reader.read(filename_queue)
+    features = tf.parse_single_example(serialized_example,
+                                       features={
+                                           'label':tf.FixedLenFeature([],tf.int64),
+                                           'img_raw':tf.FixedLenFeature([],tf.string),
+                                       })
+
+    img = tf.decode_raw(features['img_raw'],tf.uint8)
+    img = tf.reshape([224,224,3])
+    img = tf.cast(img,tf.float32)*(1./255)-0.5
+    label = tf.cast(features['label'],tf.int32)
+
+    return img,label
+
+if __name__ == '__main__':
+    img,label = read_and_decode("train.tfrecords")
+
+    img_batch, label_batch = tf.train.shuffle_batch([img,label],
+                                                    batch_size=1,
+                                                    capacity=2000,
+                                                    min_after_dequeue=1000)
+    init = tf.initialize_all_variables()
+
+    with tf.Session as sess:
+        sess.run(init)
+
+        threads = tf.train.start_queue_runners(sess=sess)
+        for i in range(3):
+            val, l = sess.run([img_batch,label_batch])
+            print (val.shape,1)
