0ab9ea9fc99888d48b48c0569ce9716be19544ad
BrilliantLing <name321@qq.com>
2016-11-26 16:10:25 +0800

    update model saver

--- Source/MNIST/mnist_cnn.py
+++ Source/MNIST/mnist_cnn.py
@@ -76,7 +76,8 @@ def main(_):
             print("step %d, training accuracy %g" %(i, train_accuracy))
         train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
 
-    saver = tf.
+    saver = tf.train.Saver()
+    save_path = tf.train(sess,"/.")
 
     print("test accuracy %g" %accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
 
