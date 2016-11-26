bde92ec5d4b10c376b2b1e1236cedd3dba1f7eb8
BrilliantLing <name321@qq.com>
2016-11-26 16:25:56 +0800

    fix some bugs

--- Source/MNIST/mnist_cnn.py
+++ Source/MNIST/mnist_cnn.py
@@ -46,15 +46,17 @@ def main(_):
     h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
     h_pool2 = max_pool_2x2(h_conv2)
 
+    """
     W_conv3 = weight_variable([5, 5, 64, 128])
     b_conv3 = bias_variable([128])
     h_conv3 = tf.nn.relu(conv2d(h_pool2, W_conv3) + b_conv3)
     h_pool3 = max_pool_2x2(h_conv3)
+    """
 
-    W_fc1 = weight_variable([7 * 7 * 128, 1024])
+    W_fc1 = weight_variable([7 * 7 * 64, 1024])
     b_fc1 = bias_variable([1024])
-    h_pool3_flat = tf.reshape(h_pool3, [-1, 7*7*128])
-    h_fc1 = tf.nn.relu(tf.matmul(h_pool3_flat, W_fc1) + b_fc1)
+    h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 64])
+    h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
     keep_prob = tf.placeholder("float")
     h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
