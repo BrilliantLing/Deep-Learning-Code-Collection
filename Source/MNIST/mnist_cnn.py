7a0f450798868a68a06c7abd0e1f23aeed15a13b
BrilliantLing <name321@qq.com>
2016-11-26 16:30:37 +0800

    fix some bugs

--- Source/MNIST/mnist_cnn.py
+++ Source/MNIST/mnist_cnn.py
@@ -10,7 +10,11 @@ from tensorflow.examples.tutorials.mnist import input_data
 
 import tensorflow as tf
 
+import os
+
 FLAGS = None
+
+model_path = os.getcwd() + '\model.ckpt'
 
 def main(_):
     mnist = input_data.read_data_sets(FLAGS.data_dir, one_hot=True)
@@ -79,7 +83,7 @@ def main(_):
         train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
 
     saver = tf.train.Saver()
-    save_path = tf.train(sess,"/.")
+    save_path = tf.train(sess,model_path)
 
     print("test accuracy %g" %accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
 
