9d9f90355d47d9d27e1ac3a7633bcff961d9b018
BrilliantLing <name321@qq.com>
2016-11-26 16:33:17 +0800

    fix some bugs

--- Source/MNIST/mnist_cnn.py
+++ Source/MNIST/mnist_cnn.py
@@ -83,7 +83,7 @@ def main(_):
         train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
 
     saver = tf.train.Saver()
-    save_path = tf.train(sess,model_path)
+    save_path = saver.save(sess,model_path)
 
     print("test accuracy %g" %accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
 
