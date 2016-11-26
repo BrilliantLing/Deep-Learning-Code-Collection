8f1a274282a1422a3e58266ef486e2a1f47936d3
BrilliantLing <name321@qq.com>
2016-11-26 17:29:06 +0800

    delete one convolutional layer

--- Source/MNIST/mnist_cnn.py
+++ Source/MNIST/mnist_cnn.py
@@ -40,24 +40,24 @@ def main(_):
 
     x_image = tf.reshape(x, [-1,28,28,1])
 
-    W_conv1 = weight_variable([5, 5, 1, 32])
+    W_conv1 = weight_variable([3, 3, 1, 32])
     b_conv1 = bias_variable([32])
     h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
     h_pool1 = max_pool_2x2(h_conv1)
 
-    W_conv2 = weight_variable([5, 5, 32, 64])
+    W_conv2 = weight_variable([3, 3, 32, 64])
     b_conv2 = bias_variable([64])
     h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
     h_pool2 = max_pool_2x2(h_conv2)
 
-    #"""
-    W_conv3 = weight_variable([5, 5, 64, 128])
+    """
+    W_conv3 = weight_variable([3, 3, 64, 128])
     b_conv3 = bias_variable([128])
     h_conv3 = tf.nn.relu(conv2d(h_pool2, W_conv3) + b_conv3)
     h_pool3 = max_pool_2x2(h_conv3)
-    #"""
+    """
 
-    W_fc1 = weight_variable([28 * 28 * 128, 1024])
+    W_fc1 = weight_variable([7 * 7 * 128, 1024])
     b_fc1 = bias_variable([1024])
     #h_pool3_flat = tf.reshape(h_pool3, [-1, 7 * 7 * 128])
     h_fc1 = tf.nn.relu(tf.matmul(h_pool3, W_fc1) + b_fc1)
