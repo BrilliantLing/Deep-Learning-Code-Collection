6c3612b9bece317dbe7b23b5e8cb7f804a1d14d0
BrilliantLing <name321@qq.com>
2016-12-06 21:38:07 +0800

    update feature map summary

--- Source/MNIST/mnist_cnn.py
+++ Source/MNIST/mnist_cnn.py
@@ -41,13 +41,14 @@ def main(_):
     
     with tf.name_scope('input_reshape'):
         x_image = tf.reshape(x, [-1,28,28,1])
-        tf.image_summary('input',x_image,100)
+        tf.image_summary('input',x_image,25)
 
     with tf.name_scope('conv_layer1'):
         W_conv1 = weight_variable([5, 5, 1, 32])
         b_conv1 = bias_variable([32])
         h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
         h_pool1 = max_pool_2x2(h_conv1)
+        tf.image_summary('feature1',h_pool1,25)
          
     with tf.name_scope('conv_layer2'):
         W_conv2 = weight_variable([5, 5, 32, 64])
