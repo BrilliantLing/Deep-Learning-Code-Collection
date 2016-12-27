a60e807aab6a4afdf5189c28622360ef234952f1
BrilliantLing <name321@qq.com>
2016-12-27 21:30:37 +0800

    finish read cifar

--- Source/CNN for Logo/ReadCifar10.py
+++ Source/CNN for Logo/ReadCifar10.py
@@ -27,4 +27,14 @@ def read_cifar10(filename_queue):
     #使用定长记录阅读器读取cifar数据集文件的图像
     reader = tf.FixedLengthRecordReader(record_bytes=record_bytes)
     result.key, value = reader.read(filename_queue)
+    #将key由字节串转换成uint8向量
+    record_bytes = tf.decode_raw(value,tf.uint8)
+    #将第一个字节由uint8转换成int32
+    result.label = tf.cast(tf.slice(record_bytes,[0],[label_bytes],tf.int32))
+    #剩余的字节是图片，将起形状用reshape函数改成[depth,height,width]
+    depth_major = tf.reshape(tf.slice(record_bytes,[label_bytes],[image_bytes]),[result.depth,result.height,result.width])
+    #将[d,h,w]转化成[h,w,d]
+    result.uint8image = tf.transpose(depth_major,[1,2,0])
+
+    return result
     
