e02dadc5bcfd879e8a73ba2923fc9fae6f53906e
BrilliantLing <name321@qq.com>
2016-12-27 22:49:33 +0800

    add generate batch function

--- Source/CNN for Logo/ReadCifar10.py
+++ Source/CNN for Logo/ReadCifar10.py
@@ -38,3 +38,23 @@ def read_cifar10(filename_queue):
 
     return result
     
+def _generate_image_and_label_batch(image,label,min_queue_examples,batch_size,shuffle):
+    num_preprocess_threads = 12
+    if shuffle:
+        image_batch,label_batch = tf.train.shuffle_batch(
+            [image,label],
+            batch_size=batch_size,
+            num_threads=num_preprocess_threads,
+            capacity=min_queue_examples + 3 * batch_size,
+            min_after_dequeue = min_queue_examples)
+    else:
+        image_batch,label_batch = tf.train.batch(
+            [image,label],
+            batch_size=batch_size,
+            num_threads=num_preprocess_threads,
+            capacity=min_queue_examples + 3 * batch_size
+        )
+    
+    tf.image_summary("images",image_batch)
+
+    return image_batch,tf.reshape(label_batch,[batch_size])
