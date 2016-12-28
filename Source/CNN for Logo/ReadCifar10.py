d9a276879a06ee9f48ff90d2c1c07e0d782b2446
BrilliantLing <name321@qq.com>
2016-12-28 15:49:56 +0800

    add function for distort images

--- Source/CNN for Logo/ReadCifar10.py
+++ Source/CNN for Logo/ReadCifar10.py
@@ -14,8 +14,8 @@ NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN = 50000
 NUM_EXAMPLES_PER_EPOCH_FOR_EVAL = 10000
 
 def read_cifar10(filename_queue):
-"""
-"""
+    """
+    """
     class CIFAR10Record(object):
         pass
     result = CIFAR10Record()
@@ -62,3 +62,41 @@ def _generate_image_and_label_batch(image,label,min_queue_examples,batch_size,sh
     tf.image_summary("images",image_batch)
 
     return image_batch,tf.reshape(label_batch,[batch_size])
+
+def distorted_inputs(data_dir,batch_size):
+    """
+    """
+    filenames = [os.path.join(data_dir,'data_batch_%d' %i)
+                 for i in xange(1,5)]
+    filenames.append(os.path.join(data_dir,'test_batch'))
+
+    for f in filenames:
+        if not tf.gfile.Exists(f):
+            raise ValueError('Failed to find file: ' +f)
+
+    filename_queue = tf.train.string_input_producer(filenames)
+
+    read_input = read_cifar10(filename_queue)
+
+    reshaped_image = tf.cast(read_input.uint8image,tf.float32)
+
+    height = IMAGE_SIZE
+    width = IMAGE_SIZE
+
+    distorted_image = tf.random_crop(reshaped_image,[height,width,3])
+
+    distorted_image = tf.image.random_flip_left_right(distorted_image)
+    distorted_image = tf.image.random_brightness(distorted_image,max_delta=63)
+    distorted_image = tf.image.random_contrast(distorted_image,lower=0.2,upper=1.8)
+    
+    float_image = tf.image.per_image_standardization(distorted_image)
+
+    min_fraction_of_examples_queue = 0.4
+    min_queue_examples = int(NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN*min_fraction_of_examples_queue)
+
+    print('Filling the queue with %d CIFAR images before starting to train.'
+          'This will take a few minutes' %min_queue_examples)
+
+    return _generate_image_and_label_batch(float_image,read_input.label,
+                                           min_queue_examples,batch_size,
+                                           shuffle=True)
