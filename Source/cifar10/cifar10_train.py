e6ed59a36a61f25350f37d4c04a20cc640afc38f
BrilliantLing <name321@qq.com>
2016-12-31 16:45:34 +0800

    fix some bugs

--- Source/cifar10/cifar10_train.py
+++ Source/cifar10/cifar10_train.py
@@ -123,10 +123,10 @@ def train():
 
 
 def main(argv=None):  # pylint: disable=unused-argument
-  cifar10.maybe_download_and_extract()
-  if tf.gfile.Exists(FLAGS.train_dir):
-    tf.gfile.DeleteRecursively(FLAGS.train_dir)
-  tf.gfile.MakeDirs(FLAGS.train_dir)
+  #cifar10.maybe_download_and_extract()
+  #if tf.gfile.Exists(FLAGS.train_dir):
+  #  tf.gfile.DeleteRecursively(FLAGS.train_dir)
+  #tf.gfile.MakeDirs(FLAGS.train_dir)
   train()
 
 
