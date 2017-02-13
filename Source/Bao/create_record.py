61b52b45c7347741c6f95e808d22f83eb6d97b98
BrilliantLing <name321@qq.com>
2017-02-13 19:42:34 +0800

    fix some bug

--- Source/Bao/create_record.py
+++ Source/Bao/create_record.py
@@ -9,6 +9,12 @@ from six.moves import xrange
 import tensorflow as tf
 from PIL import Image
 
+def _int64_feature(value):
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+
+def _bytes_feature(value):
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+
 def create_records(data_dir):
     cwd = os.getcwd()
     writer = tf.python_io.TFRecordWriter(cwd+'/logo.tfrecords')
