91358b67961e3f905b151385f12fde126017da2d
BrilliantLing <name321@qq.com>
2017-02-13 19:51:02 +0800

    fix bug

--- Source/Bao/create_record.py
+++ Source/Bao/create_record.py
@@ -13,7 +13,7 @@ def _int64_feature(value):
     return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
 
 def _bytes_feature(value):
-    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value])
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
 
 def create_records(data_dir):
     cwd = os.getcwd()
