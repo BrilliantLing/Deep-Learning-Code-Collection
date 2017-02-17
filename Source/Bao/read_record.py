96a704f29b7028aea6ef4d728aa4fc2d1836a312
BrilliantLing <name321@qq.com>
2017-02-17 14:34:23 +0800

    add

---
+++ Source/Bao/read_record.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+
+from six.moves import xrange
+import tensorflow as tf
+from PIL import Image
+
+def read_and_decode(filename):
+	#根据文件名生成一个队列
+    filename_queue = tf.train.string_input_producer([filename])
+    print filename_queue
+    exit()
+    reader = tf.TFRecordReader()
+    _, serialized_example = reader.read(filename_queue)   #返回文件名和文件
+    features = tf.parse_single_example(serialized_example,
+                                       features={
+                                           'label': tf.FixedLenFeature([], tf.int64),
+                                           'img_raw' : tf.FixedLenFeature([], tf.string),
+                                       })
+    img = tf.decode_raw(features['img_raw'], tf.uint8)
+    img = tf.reshape(img, [224, 224, 3])
+    img = tf.cast(img, tf.float32) * (1. / 255) - 0.5
+    label = tf.cast(features['label'], tf.int32)
+    return img, label
+
+read_and_decode('logo.tfrecords')
