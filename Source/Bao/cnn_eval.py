6b859116f9cb4aa5e46d4b8cc04f933e237df5ad
BrilliantLing <name321@qq.com>
2017-02-21 16:06:40 +0800

    add cnn_eval

---
+++ Source/Bao/cnn_eval.py
@@ -0,0 +1,99 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from datetime import datetime
+import math
+import time
+
+import numpy as np
+import tensorflow as tf
+
+import cnn
+import read_record
+
+FLAGS = tf.app.flags.FLAGS
+
+tf.app.flags.DEFINE_string('eval_dir', '/tmp/cifar10_eval',
+                           """Directory where to write event logs.""")
+tf.app.flags.DEFINE_string('eval_data', 'test',
+                           """Either 'test' or 'train_eval'.""")
+tf.app.flags.DEFINE_string('checkpoint_dir', '/media/storage/Data/traffic_sign_train',
+                           """Directory where to read model checkpoints.""")
+tf.app.flags.DEFINE_integer('eval_interval_secs', 60 * 5,
+                            """How often to run the eval.""")
+tf.app.flags.DEFINE_integer('num_examples', 344,
+                            """Number of examples to run.""")
+tf.app.flags.D0EFINE_boolean('run_once', False,
+                         """Whether to run eval only once.""")
+
+def eval(saver, summary_writer, top_k_top, summary_op):
+    with tf.Session() as sess:
+        ckpt = tf.train.get_checkpoint_state(FLAGS.checkpoint_dir)
+        if ckpt and ckpt.model_checkpoint_path:
+            saver.restore(sess,ckpt.model_checkpoint_path)
+            global_step = ckpt.model_checkpoint_path.split('/')[-1].split('-')[-1]
+        else:
+            print('No checkpoint file found')
+            return
+        
+        coord = tf.train.Coordinator()
+        try:
+            threads = []
+            for qr in tf.get_collection(tf.GraphKeys.QUEUE_RUNNERS):
+                threads.extend(qr.create_threads(sess, coord=coord, daemon=True, start=True))
+
+            num_iter = int(math.ceil(FLAGS.num_examples / FLAGS.batch_size))
+            true_count = 0
+            total_sample_count = num_iter * FLAGS.batch_size
+            step = 0
+            while step < num_iter and not coord.should_stop():
+                predictions = sess.run([top_k_op])
+                true_count += np.sum(predictions)
+                step += 1
+
+            precision = true_count / total_sample_count
+            print('%s: precision @ 1 = %.3f' %(datetime.now(),precision))
+
+            #summary = tf.Summary()
+            #summary.ParseFromString(sess.run(summary_op))
+            #summary.value.add(tag='Precision @ 1', simple_value=precision)
+            #summary_writer.add_
+
+        except Exception as e:
+            coord.request_stop(e)
+
+        coord.request_stop()
+        coord.join(threads, stop_grace_period_secs=10)
+
+def evaluate():
+    with tf.Graph().as_default as g:
+        eval_data = FLAGS.eval_data == 'test'
+        images, labels = read_record.read_and_decode('/media/storage/Data/traffic_sign_data/logo.tfrecords')
+        image_batch, label_batch = cnn.inputs(images,labels,FLAGS.batch_size)
+
+        logits = cnn.cnn_model(image_batch)
+
+        top_k_op = tf.nn.in_top_k(logits,label_batch,1)
+
+        variables_averages = tf.train.ExponentialMovingAverage(
+            cnn.MOVING_AVERAGE_DECAY
+        )
+        variables_to_restore = variable_averages.variables_to_restore()
+        saver = tf.train.Saver(variables_to_restore)
+        summary_op = tf.summary.merge_all()
+
+        summary_writer = tf.summary.FileWriter(FLAGS.eval_dir, g)
+
+        while True:
+            eval_once(saver, summary_writer, top_k_op, summary_op)
+            if FLAGS.run_once:
+                break
+            time.sleep(FLAGS.eval_interval_secs)
+
+def main(argv=None):
+    evaluate()
+
+if __name__ == '__main__':
+    tf.app.run()
