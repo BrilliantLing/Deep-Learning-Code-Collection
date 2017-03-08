7ea422d880726307f7050a8d00b14ce336d775da
BrilliantLing <name321@qq.com>
2017-03-08 14:59:13 +0800

    modify read mat

--- Source/cnn_traffic/read_mat.py
+++ Source/cnn_traffic/read_mat.py
@@ -21,7 +21,10 @@ from PIL import Image
 FLAGS = tf.app.flags.FLAGS
 
 #the dirctories that the mat files is stored
-flow_data_dir = 'D:\\MasterDL\\data_set\\traffic_data\\2011_flow\\'
+tp_flow_data_dir = 'D:\\MasterDL\\data_set\\traffic_data\\2011_flow_train_prediction\\'
+ep_flow_data_dir = 'D:\\MasterDL\\data_set\\traffic_data\\2011_flow_test_prediction\\'
+tr_flow_data_dir = 'D:\\MasterDL\\data_set\\traffic_data\\2011_flow_train_reality\\'
+er_flow_data_dir = 'D:\\MasterDL\\data_set\\traffic_data\\2011_flow_test_reality\\'
 speed_data_dir = 'D:\\MasterDL\\data_set\\traffic_data\\2011_speed\\'
 occupancy_data_dir = 'D:\\MasterDL\\data_set\\traffic_data\\2011_occupancy\\'
 
@@ -63,6 +66,9 @@ def merge_3channels(mat_a, mat_b, mat_c, shape):
 def _int64list_feature(value):
   return tf.train.Feature(int64_list=tf.train.Int64List(value=value))
 
+def _bytes_feature(value):
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+
 def create_tfrecord(data_dir, target_dir, record_name, variable_name):
     if(os.path.exists(target_dir + record_name)):
         print('the tfrecord file exist, it will be deleted')
@@ -71,22 +77,31 @@ def create_tfrecord(data_dir, target_dir, record_name, variable_name):
     for matfile in os.listdir(data_dir):
         mat = sio.loadmat(data_dir + matfile)
         mat = mat[variable_name]
+        mean=mat.mean()
+        stddev = mat.std()
         mat = preprocessing.scale(mat)
+        
         raw = mat.tostring()
+        #print(raw)
         example = tf.train.Example(
             features = tf.train.Features(
                 feature = {
-                    'flow':_int64list_feature(raw)
+                    'flow':_bytes_feature(raw)
                 }
             )
         )
         writer.write(example.SerializeToString())
         print(matfile + ' have been processed!')
     writer.close()
+    return mean,stddev
 
 
 def main():
-    create_tfrecord(flow_data_dir, target_dir, 'traffic_flow.tfrecords','liuliang')
+    #create_tfrecord(tp_flow_data_dir, target_dir, 'tp_traffic_flow.tfrecords','liuliang')
+    #create_tfrecord(ep_flow_data_dir, target_dir, 'ep_traffic_flow.tfrecords','liuliang')
+    create_tfrecord(tr_flow_data_dir, target_dir, 'tr_traffic_flow.tfrecords','liuliang')
+    create_tfrecord(er_flow_data_dir, target_dir, 'er_traffic_flow.tfrecords','liuliang')
+
     pass
 
 if __name__ == '__main__':
