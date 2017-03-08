20455584e1cd2b43319cdcf6da04d218773a2f30
BrilliantLing <name321@qq.com>
2017-03-08 15:02:17 +0800

    add some option

---
+++ Source/cnn_traffic/cnn_train.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import tensorflow as tf
+#import cnn
+import cnn_contrast
+import utils
+
+import argparse
+
+from datetime import datetime
+import time
+
+import os
+
+import numpy as np
+from six.moves import xrange
+import tensorflow as tf
+
+FLAGS = tf.app.flags.FLAGS
+tf.app.flags.DEFINE_string('train_dir','D:\\MasterDL\\data_set\\traffic_data\\flow_train_data\\',"""The directory where the Train data stored""")
+tf.app.flags.DEFINE_string('train_prediction_filename','tp_traffic_flow.tfrecords',"""The name of tfrecords file for train""")
+tf.app.flags.DEFINE_string('train_reality_filename', 'tr_traffic_flow.tfrecords', """The name of tfrecords file which is """)
+tf.app.flags.DEFINE_integer('max_steps',5000,"""The max steps the train process will run""")
+
+def train():
+    with tf.Graph().as_default():
+        mat_batch = utils.inputs(FLAGS.data_dir+FLAGS.train_prediction_filename, FLAGS.batch_size, 64)
+        reality = utils.read_and_decode(FLAGS.data_dir+FLAGS.train_reality_filename,'flow')
+        reality = tf.reshape(reality, [1, 288*35])
+
+        print(reality.shape)
+
+        logits = cnn_contrast.cnn_model_1(mat_batch)
+
+        print(logits.shape)
+
+        loss = utils.mse_loss(logits, reality)
+        
+        opt = tf.train.GradientDescentOptimizer(0.01)
+        train_step = opt.minimize(loss)
+
+        init = tf.global_variables_initializer()
+
+        summary_op = tf.summary.merge_all()
+
+        saver = tf.train.Saver()
+
+        sess = tf.Session()
+        sess.run(init)
+
+        tf.train.start_queue_runners(sess=sess)
+
+        summary_writer = tf.summary.FileWriter('D:\\MasterDL\\data_set\\traffic_data\\flow_train_data')
+        for step in xrange(FLAGS.max_steps):
+            start_time = time.time()
+            _, loss_value = sess.run([train_step, loss])
+            duration = time.time() - start_time
+
+            assert not np.isnan(loss_value), 'Model diverged with loss = NaN'
+
+            if step % 100 == 0:
+                num_examples_per_step = FLAGS.batch_size
+                examples_per_sec = num_examples_per_step / duration
+                sec_per_batch = float(duration)
+                format_str = ('%s: step %d, loss = %.2f (%.1f examples/sec; %.3f sec/batch)')
+                print (format_str % (datetime.now(), step, loss_value, examples_per_sec, sec_per_batch))
+                summary_str = sess.run(summary_op)
+                summary_writer.add_summary(summary_str, step)
+            
+            if step % 1000 == 0:
+                checkpoint_path = os.path.join(FLAGS.train_dir, 'cnn_model.ckpt')
+                saver.save(sess, checkpoint_path, global_step=step)
+
+def main(_):
+    train()
+
+if __name__ == '__main__':
+    tf.app.run()
