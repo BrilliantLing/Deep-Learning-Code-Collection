ff7f22138670629788c3d2d5a93cad1be7f4edc0
BrilliantLing <name321@qq.com>
2017-03-13 21:09:49 +0800

    update

--- Source/cnn_traffic/read_mat.py
+++ Source/cnn_traffic/read_mat.py
@@ -25,7 +25,10 @@ tp_speed_data_dir = 'D:\\MasterDL\\data_set\\traffic_data\\2011_speed_train_pred
 ep_speed_data_dir = 'D:\\MasterDL\\data_set\\traffic_data\\2011_speed_test_prediction\\'
 tr_speed_data_dir = 'D:\\MasterDL\\data_set\\traffic_data\\2011_speed_train_reality\\'
 er_speed_data_dir = 'D:\\MasterDL\\data_set\\traffic_data\\2011_speed_test_reality\\'
-speed_data_dir = 'D:\\MasterDL\\data_set\\traffic_data\\2011_speed\\'
+tp_flow_data_dir = 'D:\\MasterDL\\data_set\\traffic_data\\2011_flow\\2011_flow_train_prediction\\'
+ep_flow_data_dir = 'D:\\MasterDL\\data_set\\traffic_data\\2011_flow\\2011_flow_test_prediction\\'
+tr_flow_data_dir = 'D:\\MasterDL\\data_set\\traffic_data\\2011_flow\\2011_flow_train_reality\\'
+er_flow_data_dir = 'D:\\MasterDL\\data_set\\traffic_data\\2011_flow\\2011_flow_test_reality\\'
 occupancy_data_dir = 'D:\\MasterDL\\data_set\\traffic_data\\2011_occupancy\\'
 
 #the dirctories that the numpy files or tfrecords files to be saved
@@ -69,7 +72,23 @@ def _int64list_feature(value):
 def _bytes_feature(value):
     return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
 
-def create_tfrecord(data_dir, target_dir, record_name, variable_name):
+def flow_matrix_process(matrix):
+    matrix = np.delete(matrix, 2, 0)
+    matrix = np.delete(matrix, 29, 0)
+    matrix = np.delete(matrix, 28, 0)
+    matrix = matrix[:,72:288]
+    for i in range(matrix.shape[0]):
+        for j in range(matrix.shape[1]):
+            if matrix[i][j] == 0:
+                matrix[i][j] = 1
+    for i in range(matrix.shape[0]):
+        for j in range(matrix.shape[1]):
+            if j % 3 ==0:
+                matrix[i][j] = matrix[i][j] + matrix[i][j+1] +matrix[i][j+2]
+    matrix = matrix[:,::3]
+    return matrix
+
+def create_tfrecord(data_dir, target_dir, record_name, variable_name, feature_name, process):
     if(os.path.exists(target_dir + record_name)):
         print('the tfrecord file exist, it will be deleted')
         os.remove(target_dir + record_name)
@@ -77,13 +96,9 @@ def create_tfrecord(data_dir, target_dir, record_name, variable_name):
     for matfile in os.listdir(data_dir):
         mat = sio.loadmat(data_dir + matfile)
         mat = mat[variable_name]
-        mat = np.delete(mat, 2, 0)
-        mat = np.delete(mat, 29, 0)
-        mat = np.delete(mat, 28, 0)
-        mat = mat[:,72:288]
-        #mat = mat[:,::3]
+        mat = process(mat)
         #print(mat)
-        print(mat.shape)
+        #print(mat.shape)
         mean=mat.mean()
         stddev = mat.std()
         mat = preprocessing.scale(mat)
@@ -93,7 +108,7 @@ def create_tfrecord(data_dir, target_dir, record_name, variable_name):
         example = tf.train.Example(
             features = tf.train.Features(
                 feature = {
-                    'speed':_bytes_feature(raw)
+                    feature_name:_bytes_feature(raw)
                 }
             )
         )
@@ -104,11 +119,11 @@ def create_tfrecord(data_dir, target_dir, record_name, variable_name):
 
 
 def main():
-    create_tfrecord(tp_flow_data_dir, target_dir, 'tp_traffic_speed.tfrecords','sudo')
-    mean, stddev = create_tfrecord(ep_flow_data_dir, target_dir, 'ep_traffic_speed.tfrecords','sudo')
+    create_tfrecord(tp_flow_data_dir, target_dir, 'tp_traffic_flow.tfrecords','liuliang', 'flow', flow_matrix_process)
+    mean, stddev = create_tfrecord(ep_flow_data_dir, target_dir, 'ep_traffic_flow.tfrecords','liuliang', 'flow', flow_matrix_process)
     print(mean,stddev)
-    create_tfrecord(tr_flow_data_dir, target_dir, 'tr_traffic_speed.tfrecords','sudo')
-    mean, stddev = create_tfrecord(er_flow_data_dir, target_dir, 'er_traffic_speed.tfrecords','sudo')
+    create_tfrecord(tr_flow_data_dir, target_dir, 'tr_traffic_flow.tfrecords','liuliang', 'flow', flow_matrix_process)
+    mean, stddev = create_tfrecord(er_flow_data_dir, target_dir, 'er_traffic_flow.tfrecords','liuliang', 'flow', flow_matrix_process)
     print(mean,stddev)
 
     pass
