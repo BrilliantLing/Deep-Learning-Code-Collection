d5087c476172b9efa0b6858726e31c08de155671
BrilliantLing <name321@qq.com>
2017-04-04 19:41:12 +0800

    add record.py

---
+++ Source/cnn_traffic/record.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-  
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import numpy as np
+import scipy.io as sio
+import os
+
+from sklearn import preprocessing
+
+import tensorflow as tf
+
+import argparse
+
+from six.moves import xrange
+import tensorflow as tf
+from PIL import Image
+
