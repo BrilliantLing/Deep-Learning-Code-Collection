bfb378d49af89ba1b446c08549632e9c84f38e79
BrilliantLing <name321@qq.com>
2017-04-04 19:32:09 +0800

    make some changes

--- Source/cnn_traffic/utils.py
+++ Source/cnn_traffic/utils.py
@@ -10,20 +10,3 @@ import os
 import scipy.io as sio
 import numpy as np
 import tensorflow as tf
-
-def read_matfile(filename, variable_name):
-    mat = sio.loadmat(file_name)
-    mat = mat[variable_name]
-    return mat
-
-def read_matfile_from_dir(data_dir,variable_name, shape):
-    mat_array=np.array([])
-    for filename in os.listdir(data_dir):
-        mat = read_matfile(filename, variable_name)
-        mat_array = np.append(mat_array, mat)
-    mat_array = np.reshape(mat_array, shape)
-    return mat_array
-
-def create_numpy_record(mat_array, target_dir, filename):
-    np.save(mat_array, target_dir+filename)
-
