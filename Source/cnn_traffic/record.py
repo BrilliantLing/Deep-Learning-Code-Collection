853e1a551b50329eaf82f0ae6bf04ebf07dbc7d1
BrilliantLing <name321@qq.com>
2017-04-12 22:58:01 +0800

    add functions

--- Source/cnn_traffic/record.py
+++ Source/cnn_traffic/record.py
@@ -40,17 +40,18 @@ def create_tfrecord(data_dirs, target_dir, record_name, variable_name, low_proce
         today_data = sio.loadmat(data_dirs[0]+today_filenames[i])
         today_data = today_data[variable_name]
         low_today = low_process(today_data, [2, 29, 28], 72, 288)
-        low_today = pp.normalize(low_today)
+        low_today, _, _ = pp.normalize(low_today)
         mid_today = mid_process(today_data, [2, 29, 28], 72, 288)
-        mid_today = pp.normalize(mid_today)
+        mid_today, _, _  = pp.normalize(mid_today)
         high_today = high_process(today_data, [2, 29, 28], 72, 288)
-        high_today = pp.normalize(high_today)
+        high_today, _, _  = pp.normalize(high_today)
         low_today = low_today.tostring()
         mid_today = mid_today.tostring()
         high_today = high_today.tostring()
         tomorrow_data = sio.loadmat(data_dirs[1]+tomorrow_filenames[i])
         tomorrow_data = tomorrow_data[variable_name]
         mid_tomorrow = mid_process(tomorrow_data, [2, 29, 28], 72, 288)
+        mid_tomorrow, _, _ = pp.normalize(mid_tomorrow)
         mid_tomorrow = mid_tomorrow.tostring()
         example = tf.train.Example(
             features = tf.train.Features(
@@ -73,19 +74,37 @@ def create_tfrecord_default(data_dirs, target_dir, record_name, variable_name, p
     writer = tf.python_io.TFRecordWriter(target_dir + record_name)
     today_filenames = os.listdir(data_dirs[0])
     tomorrow_filenames = os.listdir(data_dirs[1])
-    today_max = []
-    today_min = []
-    tomorrow_max = []
-    tomorrow_min = []
+    today_max_list = []
+    today_min_list = []
+    tomorrow_max_list = []
+    tomorrow_min_list = []
     for i in range(len(today_filenames)):
         today_data = sio.loadmat(data_dirs[0]+today_filenames[i])
         today_data = today_data[variable_name]
         today = process(today_data, [2, 29, 28], 72, 288)
+        today, today_max, today_min = pp.normalize(today)
+        today_max_list.append(today_max)
+        today_min_list.append(today_min)
         today = today.tostring()
         tomorrow_data = sio.loadmat(data_dirs[1]+tomorrow_filenames[i])
         tomorrow_data = tomorrow_data[i]
         tomorrow = process(tomorrow_data, [2, 29, 28], 72, 288)
+        tomorrow, tomorrow_max, tomorrow_min = pp.normalize(tomorrow)
+        tomorrow_max_list.append(tomorrow_max)
+        tomorrow_min_list.append(tomorrow_min)
         tomorrow = tomorrow.tostring()
+        example = tf.train.Example(
+            features = tf.train.Features(
+                feature = {
+                    'today':_bytes_feature(today),
+                    'tomorrow':_bytes_feature(tomorrow)
+                }
+            )
+        )
+        writer.write(example.SerializeToString())
+        print('today:'+today_filenames[i]+' tomorrow:'+tomorrow_filenames[i]+' have been processed.')
+    writer.close()
+    return today_max_list, today_min_list, tomorrow_max_list, tomorrow_min_list
 
 
 
