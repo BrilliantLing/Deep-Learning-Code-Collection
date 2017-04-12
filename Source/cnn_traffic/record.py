195bf883ed72f032ff150982c6fc9f2fe685bf9a
BrilliantLing <name321@qq.com>
2017-04-12 22:40:21 +0800

    add two create function

--- Source/cnn_traffic/record.py
+++ Source/cnn_traffic/record.py
@@ -8,7 +8,7 @@ import numpy as np
 import scipy.io as sio
 import os
 
-from sklearn import preprocessing
+#from sklearn import preprocessing
 
 import tensorflow as tf
 
@@ -17,6 +17,8 @@ import argparse
 from six.moves import xrange
 import tensorflow as tf
 from PIL import Image
+
+import preprocess as pp
 
 def create_numpy_record(matrix, target_dir, filename):
     np.save(matrix, target_dir+filename)
@@ -27,45 +29,65 @@ def _int64list_feature(value):
 def _bytes_feature(value):
     return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
 
-def create_tfrecord(data_dirs, target_dir, record_name, variable_name, process):
+def create_tfrecord(data_dirs, target_dir, record_name, variable_name, low_process, mid_process, high_process):
     if(os.path.exists(target_dir + record_name)):
         print('The tfrecord file exist, it will be deleted')
         os.remove(target_dir + record_name)
     writer = tf.python_io.TFRecordWriter(target_dir + record_name)
     today_filenames = os.listdir(data_dirs[0])
     tomorrow_filenames = os.listdir(data_dirs[1])
-    today_max_list = []
-    today_min_list = []
-    tomorrow_max_list = []
-    tomorrow_min_list = []
     for i in range(len(today_filenames)):
         today_data = sio.loadmat(data_dirs[0]+today_filenames[i])
+        today_data = today_data[variable_name]
+        low_today = low_process(today_data, [2, 29, 28], 72, 288)
+        low_today = pp.normalize(low_today)
+        mid_today = mid_process(today_data, [2, 29, 28], 72, 288)
+        mid_today = pp.normalize(mid_today)
+        high_today = high_process(today_data, [2, 29, 28], 72, 288)
+        high_today = pp.normalize(high_today)
+        low_today = low_today.tostring()
+        mid_today = mid_today.tostring()
+        high_today = high_today.tostring()
         tomorrow_data = sio.loadmat(data_dirs[1]+tomorrow_filenames[i])
-        today_min = today_data.min()
-        tomorrow_min = tomorrow_data.min()
-        today_max = today_data.max()
-        tomorrow_max = tomorrow_data.max()
-        today_max_list.append(today_max)
-        today_min_list.append(today_min)
-        tomorrow_max_list.append(tomorrow_max)
-        tomorrow_min_list.append(tomorrow_min)
-        today_data = (today_data - today_min) / (today_max - today_min)
-        tomorrow_data = (tomorrow_data - tomorrow_min) / (tomorrow_max - tomorrow_min)
-        today_raw = today_data.tostring()
-        tomorrow_raw = tomorrow_data.tostring()
-
+        tomorrow_data = tomorrow_data[variable_name]
+        mid_tomorrow = mid_process(tomorrow_data, [2, 29, 28], 72, 288)
+        mid_tomorrow = mid_tomorrow.tostring()
         example = tf.train.Example(
             features = tf.train.Features(
                 feature = {
-                    "today":_bytes_feature(today_raw),
-                    "tomorrow":_bytes_feature(tomorrow_raw)
+                    'low_today':_bytes_feature(low_today),
+                    'mid_today':_bytes_feature(mid_today),
+                    'high_today':_bytes_feature(high_today)
+                    'mid_tomorrow':_bytes_feature(mid_tomorrow)
                 }
             )
         )
         writer.write(example.SerializeToString())
         print('today:'+today_filenames[i]+' tomorrow:'+tomorrow_filenames[i]+' have been processed.')
     writer.close()
-    return today_max_list, today_min_list, tomorrow_max_list, tomorrow_min_list
+
+def create_tfrecord_default(data_dirs, target_dir, record_name, variable_name, process):
+    if(os.path.exists(target_dir + record_name)):
+        print('The tfrecord file exist, it will be deleted')
+        os.remove(target_dir + record_name)
+    writer = tf.python_io.TFRecordWriter(target_dir + record_name)
+    today_filenames = os.listdir(data_dirs[0])
+    tomorrow_filenames = os.listdir(data_dirs[1])
+    today_max = []
+    today_min = []
+    tomorrow_max = []
+    tomorrow_min = []
+    for i in range(len(today_filenames)):
+        today_data = sio.loadmat(data_dirs[0]+today_filenames[i])
+        today_data = today_data[variable_name]
+        today = process(today_data, [2, 29, 28], 72, 288)
+        today = today.tostring()
+        tomorrow_data = sio.loadmat(data_dirs[1]+tomorrow_filenames[i])
+        tomorrow_data = tomorrow_data[i]
+        tomorrow = process(tomorrow_data, [2, 29, 28], 72, 288)
+        tomorrow = tomorrow.tostring()
+
+
 
 def read_and_decode(filename, shape):
     filename_queue = tf.train.string_input_producer(filename,shuffle=True)
