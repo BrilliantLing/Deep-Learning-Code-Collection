59594b0cec454a6ed74b1cd09ebf8348a14c49f1
BrilliantLing <name321@qq.com>
2017-04-13 22:58:48 +0800

    add some functions

--- Source/cnn_traffic/utils.py
+++ Source/cnn_traffic/utils.py
@@ -11,3 +11,36 @@ import scipy.io as sio
 import numpy as np
 import tensorflow as tf
 
+def _activation_summary(x):
+    """
+    """
+    tensor_name = x.op.name
+    tf.summary.histogram(tensor_name + '/activations',x)
+    tf.summary.scalar(tensor_name + '/sparsity',tf.nn.zero_fraction(x))
+
+def _variable_on_cpu(name,shape,initializer):
+    with tf.device('/cpu:0'):
+        dtype = tf.float16 if FLAGS.use_fp16 else tf.float32
+        var = tf.get_variable(name,shape,initializer=initializer,dtype=dtype)
+    return var
+
+def _variable_on_gpu(name,shape,initializer):
+    with tf.device('/gpu:0'):
+        dtype = tf.float16 if FLAGS.use_fp16 else tf.float32
+        var = tf.get_variable(name,shape,initializer=initializer,dtype=dtype)
+    return var
+
+def _variable_with_weight_decay(name,shape,stddev,wd):
+    dtype = tf.float16 if FLAGS.use_fp16 else tf.float32
+    var = _variable_on_cpu(
+        name,
+        shape,
+        tf.truncated_normal_initializer(stddev=stddev,dtype=dtype)
+    )
+    if wd is not None:
+        weigth_decay = tf.multiply(tf.nn.l2_loss(var),wd,name='weight_loss')
+        tf.add_to_collection('losses',weigth_decay)
+    return var
+
+def conv2d(input_data, kernel_width, kernel_height, inchannels, outchannels, stride=[1,1,1,1], padding='SAME', name=None):
+    pass
