6626a372167af11bf76dd229954f15060d456681
BrilliantLing <name321@qq.com>
2017-04-18 20:16:41 +0800

    change some config

--- Source/cnn_traffic/config.py
+++ Source/cnn_traffic/config.py
@@ -6,12 +6,16 @@ FLAGS = tf.app.flags.FLAGS
 tf.app.flags.DEFINE_boolean('use_fp16', False, """Train the model using fp16.""")
 tf.app.flags.DEFINE_integer('train_batch_size', 1, """The size of batch when training""")
 tf.app.flags.DEFINE_integer('test_batch_size', 1, """The size of batch when testing""")
-tf.app.flags.DEFINE_integer('epoch', 400, """The max iterations the model will be trained""")
+tf.app.flags.DEFINE_integer('epoch', 300, """The max iterations the model will be trained""")
+tf.app.flags.DEFINE_integer('num_examples_train', 319, """The examples that the training set has""")
+tf.app.flags.DEFINE_integer('num_examples_test', 39, """The examples that the test set has""")
 tf.app.flags.DEFINE_string('train_dir','D:\\MasterDL\\data_set\\traffic_data\\speed\\train_data', """The directory where the training data saved in.""")
 tf.app.flags.DEFINE_string('test_dir', 'D:\\MasterDL\\data_set\\traffic_data\\speed\\test_data', """The directory where the testing data saved in.""")
 tf.app.flags.DEFINE_string('checkpoint_dir', 'D:\\MasterDL\\data_set\\traffic_data\\speed\\checkpoint', """The directory where the checkpoint data saved in.""")
 tf.app.flags.DEFINE_string('train_input_path', 'D:\\MasterDL\\data_set\\traffic_data\\speed\\tfrecords\\train\\train.tfrecords', """The diretory where the input data for trainning saved in.""")
-tf.app.flags.DEFINE_string('test_input_path', 'D:\\MasterDL\\data_set\\traffic_data\\speed\\tfrecords\\test', """The diretory where the input data for testing saved in.""")
+tf.app.flags.DEFINE_string('test_input_path', 'D:\\MasterDL\\data_set\\traffic_data\\speed\\tfrecords\\test\\test.tfrecords', """The diretory where the input data for testing saved in.""")
+tf.app.flags.DEFINE_string('test_today_mat_dir','D:\\MasterDL\\data_set\\traffic_data\\speed\\test\\today\\',"""To be done""")
+tf.app.flags.DEFINE_string('test_tomorrow_mat_dir','D:\\MasterDL\\data_set\\traffic_data\\speed\\test\\tomorrow\\',"""To be done""")
 
 NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN = 319
 
